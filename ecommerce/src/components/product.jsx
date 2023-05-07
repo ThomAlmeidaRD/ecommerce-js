b2aa81b2ac7b21f48743435bab058d0002301598
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-07 03:38:18 -0300

    capturando informacoes dos itens

--- ecommerce/src/components/product.jsx
+++ ecommerce/src/components/product.jsx
@@ -1,13 +1,25 @@
 
-
 function Product(props) {
     return (
-        <div className="product">
+        <div className="product" onClick={openPage}>
             <img src={props.img} alt="" />
             <h2>{props.name}</h2>
             <p className="pricing">{props.price}</p>
         </div>
     )
 }
+
+
+function openPage(item) {
+    //console.log(item.target)
+    var thisElement = item.target
+    var thisParent = thisElement.parentNode
+    console.log(thisParent)
+    console.log(thisParent.childNodes[0].src)
+    console.log(thisParent.childNodes[1].innerText)
+    console.log(thisParent.childNodes[2].innerText)
+
+}
+
 
 export default Product
