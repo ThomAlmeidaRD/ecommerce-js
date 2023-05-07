836ba3f652c63fbf588b62afe221ccba678c9be6
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-07 01:27:50 -0300

    filtering [100%]

--- ecommerce/src/components/search-header.jsx
+++ ecommerce/src/components/search-header.jsx
@@ -1,18 +1,30 @@
 
 
-var txtSrc = document.getElementById('txt-search')
 function filter() {
 
     var catalogList = document.querySelectorAll('.product h2')
+    var txtSrc = document.getElementById('txt-search')
+    var btnSrc = document.getElementById('btn-search')
 
     for (var item of catalogList) {
+
 
         var item_title = item.textContent.toLocaleLowerCase()
 
         if (!item_title.includes(txtSrc.value)) {
             var product_div = item.parentNode
             product_div.style.display = 'none'
+            btnSrc.childNodes[0].name = 'close'
+            btnSrc.style.backgroundColor = 'crimson'
+            btnSrc.style.color = '#fff'
+        } else {
+            var product_div = item.parentNode
+            product_div.style.display = 'block'
+            btnSrc.childNodes[0].name = 'search'
+            btnSrc.style.backgroundColor = '#ffce00'
+            btnSrc.style.color = '#000'
         }
+
 
     }
 
